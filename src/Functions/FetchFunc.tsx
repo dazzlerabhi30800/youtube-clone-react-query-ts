119d2e0b3a1f932d0df44e68b61f63004767c6ec
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-03-05 19:36:18 +0530

    added env file & formatted time

--- src/Functions/FetchFunc.tsx
+++ src/Functions/FetchFunc.tsx
@@ -1,7 +1,7 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "11cbd1ad6fmsh0a817753b0087c5p1e4529jsn2e3582618183",
+    "X-RapidAPI-Key": import.meta.env.VITE_YOUTUBE_KEY,
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
@@ -15,17 +15,16 @@ export const fetchHome = async () => {
 };
 
 export const fetchCategory = async (category: string) => {
-  console.log(category);
   const response = await fetch(`https://youtube138.p.rapidapi.com/search/?q=${category}&hl=en&gl=US`, options);
   const result = await response.json();
   return result;
 }
 
 export const formatTime = (time: number) => {
-  const hours = Math.floor(100 / 60);
-  const minutes = Math.floor(time / 60);
+  const minutes = time >= 3600 ? Math.floor(time % 60) : Math.floor(time / 60);
+  const hours = Math.floor(time / 3600);
   const seconds = Math.floor(time % 60);
-  return `${hours >= 1 && hours}:${minutes <= 0 ? "00" : minutes}:${seconds <= 0 ? "00" : seconds}`;
+  return `${hours >= 1 ? hours + ":" : "00" + ":"}${minutes <= 0 ? "00" : minutes < 10 ? "0" + minutes : minutes}:${seconds <= 0 ? "00" : seconds < 10 ? "0" + seconds : seconds}`;
 };
 
 
